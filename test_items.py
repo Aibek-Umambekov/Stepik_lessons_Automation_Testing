19c68375b4ee89efe2da1427b460f387de9b97a8
Aibek Umambekov <33904438+Aibek-Umambekov@users.noreply.github.com>
2023-08-17 17:58:40 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+
+def test_items(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    add_to_cart_button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert add_to_cart_button, "Add to cart button is not present"
 
